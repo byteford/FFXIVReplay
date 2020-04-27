94a52d89f59689fa9102c403e5c18e96271cc863
James Sandford <sandford.james@gmail.com>
2020-04-27 10:54:08 +0100

    clean up fileProc

--- FileProc.py
+++ FileProc.py
@@ -20,36 +20,25 @@ class FileProc:
                 print(procLine)
 
             if id in data.notUse:
-                #readNextLine()
                 return True
             if id == data.ChangeZone:
                 if(data.log):
                     print("moved to: ",procLine[3])
                 self.play.clearObjs()
-                #readNextLine()
                 return False
             if id == data.ChangePrimaryPlayer:
-                #readNextLine()
                 return True
             if id == data.AddCombatant:
                 self.play.addObj(procLine)
-                #if(procLine[12] != '44' ):  
-                #    temp = self.play.addObj(procLine[2],procLine[3],float(procLine[17]),float(procLine[18]),float(procLine[19]),float(procLine[20]))
-                #    if(procLine[10] == '11540' or procLine[10] == '11541'): # Furor adds that start hidden 
-                #        temp.hide()
-                #readNextLine()
                 return True
             if id == data.NetworkUpdateHP:
                 self.play.moveObj(procLine[2].lower(),float(procLine[10]),float(procLine[11]),float(procLine[12]),float(procLine[13]))
-                #readNextLine()
                 return True
             if id== data.NetworkStatusEffects:
                 self.play.moveObj(procLine[2].lower(),float(procLine[11]),float(procLine[12]),float(procLine[13]),float(procLine[14]))
-                #readNextLine()
                 return True
             if id == data.RemoveCombatant:
                 self.play.removeObj(procLine[2])
-                #readNextLine()
                 return True
             if id == data.NetworkAbility: #can move target
                 self.play.moveObj(procLine[2].lower(),float(procLine[40]),float(procLine[41]),float(procLine[42]),float(procLine[43]))
